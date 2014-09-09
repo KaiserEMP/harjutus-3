6caaf93d8981679ba8ac85025a13fdbaad7aeb0f
KaiserEMP <kaidogta3@hotmail.com>
2014-09-09 13:46:31 +0300

    Add sample strings

--- index.php
+++ index.php
@@ -16,6 +16,18 @@
     echo "<br>";
 ?>
 
+
+	<h2> muutjad </h2>
+	<?php echo "Hello world!"; ?><br>
+	<?php echo 'Hello world!'; ?>
+
+	<br>
+	<?php
+    $variable = 24;
+    $variable2 = 26;
+    echo $variable . $variable2;
+   
+?>
 </body>
 
 </html>
