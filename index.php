266da32d061f519d9a3bcbb97433364051e98ad6
KaiserEMP <kaidogta3@hotmail.com>
2014-09-10 12:22:27 +0300

    Add sample content echo functions

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<! DOCKTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
@@ -9,25 +9,53 @@
 
 <body>
 
-<?php
-    $variable = 20;
-    echo $variable;
-    // Lisan veebilehitsejas parema loetavuse saamiseks reamurdmise elemendi:
-    echo "<br>";
-?>
-
-
-	<h2> muutjad </h2>
-	<?php echo "Hello world!"; ?><br>
-	<?php echo 'Hello world!'; ?>
-
-	<br>
 	<?php
-    $variable = 24;
-    $variable2 = 26;
-    echo $variable . $variable2;
-   
-?>
+	    $variable = 20;
+	    echo $variable;
+	    // Lisan veebilehitsejas parema loetavuse saamiseks reamurdmise elemendi:
+	    echo "<br>";
+	?>
+
+
+		<h2> muutjad </h2>
+		<?php echo "Hello world!"; ?><br>
+		<?php echo 'Hello world!'; ?>
+
+		<br>
+		<?php
+	    $variable = 24;
+	    $variable2 = 26;
+	    echo $variable . $variable2;
+	   
+	?>
+
+	<h2>Nimekirja väljastamine muutujate ja stringide abil</h2>
+
+	<?php
+		$student1 = "<li>Margit</li>";
+		$student2 = "<li>aigar</li>";
+		$student3 = "<li>asdasdasd</li>";
+		$student4 = "<li>1231231</li>";
+		$student5 = "<li>Marasdasasdgit</li>";
+
+
+
+
+	?>
+
+
+
+			<ul>
+				<?php echo $student1; ?>
+				<?php echo $student2; ?>
+				<?php echo $student3; ?>
+				<?php echo $student4; ?>
+				<?php echo $student5; ?>
+			</ul>
+
+
+
+
 </body>
 
 </html>
